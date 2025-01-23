a2075f9237936bbc717562a30ac1c6db837519af
Takmim00 <takmimm@gmail.com>
2025-01-23 22:12:24 +0600

    footer added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -25,6 +26,7 @@ export default function RootLayout({ children }) {
       >
         <Navbar />
         <main>{children}</main>
+        <Footer />
       </body>
     </html>
   );
